a7734f062113ab6d52e8a20dab4230b4dc9460b5
Penny <pmwinter2015@gmail.com>
2021-11-05 20:31:53 -0500

    Prototype 0.0.2
    ANT GAME
    ======
    
    	* Ants will now turn around when they hit walls
    	* Ants do not collide with eachother
    	* Spawner now spawns in multiple ants on a timer

--- Ant Game/Assets/Scripts/AntMover.cs
+++ Ant Game/Assets/Scripts/AntMover.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class AntMover : MonoBehaviour
 {
     Rigidbody2D ant;
+    int moveDirection = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,8 +15,29 @@ public class AntMover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+    }
+
+    private void Walk()
+    {
         var antPos = new Vector2(ant.transform.position.x, ant.transform.position.y);
 
-        transform.position = new Vector2(antPos.x + (1 * Time.deltaTime), antPos.y);
+        transform.position = new Vector2(antPos.x + (moveDirection * Time.deltaTime), antPos.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            moveDirection *= -1;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            Walk();
+        }
     }
 }
--- Ant Game/Assets/Scripts/AntSpawner.cs
+++ Ant Game/Assets/Scripts/AntSpawner.cs
@@ -1,28 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class AntSpawner : MonoBehaviour
 {
     public GameObject ant;
     public GameObject spawner;
+    public float spawnTime;
+    public int numberOfAnts;
+
+    Vector2 spawnerPos;
+    Vector2 newAntPos;
+    Quaternion spawnerRot;
+
     // Start is called before the first frame update
     void Start()
     {
-        var spawnerPos = spawner.transform.position;
-        var newAntPos = new Vector3(spawnerPos.x, spawnerPos.y);
-        var spawnerRot = spawner.transform.rotation;
-
-        GameObject newAnt = Instantiate(ant, newAntPos, spawnerRot);
-        newAnt.transform.position = spawnerPos;
+        spawnerPos = spawner.transform.position;
+        newAntPos = new Vector2(spawnerPos.x, spawnerPos.y);
+        spawnerRot = spawner.transform.rotation;
 
         Debug.Log(newAntPos + ", " + spawnerPos);
 
+        if (numberOfAnts <= 0)
+        {
+            numberOfAnts = 1;
+        };
+
+        StartCoroutine(AntSpawn(numberOfAnts));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    IEnumerator AntSpawn(int i)
+    {
+        var wait = new WaitForSeconds(spawnTime);
+        while (i > 0)
+        {
+            Instantiate(ant, newAntPos, spawnerRot);
+            i--;
+            yield return wait;
+        }
     }
 }
